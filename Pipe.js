4901ec0b34cf85bc6a9371419671a7471d14e696
nguyenkhanhhn <52152972+nguyenkhanhhn@users.noreply.github.com>
2019-07-22 21:54:19 +0700

    demo flappy bird

--- Pipe.js
+++ Pipe.js
@@ -9,26 +9,8 @@ fg.src = 'images/fg.png';
 pipeNorth.src = 'images/pipeNorth.png';
 PipeSouth.src = 'images/PipeSouth.png';
 var scor = new Audio();
-
-
-
-// imgNames = ["bg.png","pipeNorth.png","pipeSouth","fg.png", "bird.png"];
-// function loadImage() {
-//     let imgCount = 0;
-//     for (var i = 0; i < imgNames.length; i++) {
-//         let img = new Image();
-//         img.src = "images/" + imgNames[i];
-//         img.onloadedmetadata = function() {
-//             imgCount++;
-//             if (imgCount == imgNames.length) {
-//                 Oracale();
-//             }
-//         }
-//     }
-// }
 let gap = 120;
-
-let pipesize=80;
+let pipesize = 80;
 let Oracale = function (canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
@@ -42,7 +24,7 @@ let Oracale = function (canvas) {
     let score = 0;
     this.draw = function () {
         let constant = pipeNorth.height + gap;
-        for (var i = 0; i < pipe.length; i++) {
+        for (let i = 0; i < pipe.length; i++) {
             this.ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
             this.ctx.drawImage(PipeSouth, pipe[i].x, pipe[i].y + constant);
 
@@ -54,6 +36,12 @@ let Oracale = function (canvas) {
                     y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
                 });
             }
+
+        }
+    };
+    this.checkwin = function () {
+        let constant = pipeNorth.height + gap;
+        for (let i = 0; i < pipe.length; i++) {
             if (drawbird.getx() + birdd.width >= pipe[i].x
                 && drawbird.getx() <= pipe[i].x + pipeNorth.width
                 && (drawbird.gety() <= pipe[i].y + pipeNorth.height
@@ -61,26 +49,34 @@ let Oracale = function (canvas) {
                 || drawbird.gety() + birdd.height >= canvas.height - fg.height) {
                 location.reload(); // reload game
             }
+
+
+        }
+
+    };
+    this.checkscore=function () {
+        for (let i = 0; i < pipe.length; i++){
             if (pipe[i].x === drawbird.getx()) {
                 score += 1;
                 scor.play();
             }
-
-
         }
         this.ctx.fillStyle = "#000";
         this.ctx.font = "20px Verdana";
         this.ctx.fillText("Score : " + score, 30, 30);
-
-    }
+    };
 };
 let drawpipe = new Oracale(canvas);
 drawpipe.draw();
+drawpipe.checkwin();
+drawpipe.checkscore();
 
 function movebird() {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     drawbg.draw();
     drawpipe.draw();
+    drawpipe.checkwin();
+    drawpipe.checkscore();
     drawfg.draw();
     drawbird.draw();
     drawbird.move();
